214c182bd648423529b388815c24fba6b011873a
Richelly Italo <richellyitalo@gmail.com>
2020-07-18 19:21:46 -0300

    feat(plan): breadcrumb -> index

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Planos
+ */
 Route::any('admin/plans/search', 'Admin\PlanController@search')->name('plans.search');
 Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
 Route::get('admin/plans/create', 'Admin\PlanController@create')->name('plans.create');
 Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show');
 Route::post('admin/plans', 'Admin\PlanController@store')->name('plans.store');
 Route::delete('admin/plans/{url}', 'Admin\PlanController@destroy')->name('plans.destroy');
+
+Route::get('admin', 'Admin\PlanController@index')->name('admin.index');
+
 
 Route::get('/', function () {
     return view('welcome');
